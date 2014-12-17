b0a7e1c5b46a68d279f063ad7678f19e522653f1
Jan Krutina <krutijan@fel.cvut.cz>
2014-12-17 11:52:53 +0100

    fix of json response encoding

--- app/src/main/java/app/com/bisnode/requests/CustomJsonArrayRequest.java
+++ app/src/main/java/app/com/bisnode/requests/CustomJsonArrayRequest.java
@@ -1,11 +1,17 @@
 package app.com.bisnode.requests;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
 
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
+import org.json.JSONException;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +27,20 @@ public class CustomJsonArrayRequest extends JsonArrayRequest
         Map<String, String> headers = new HashMap<>();
         headers.put("sid", "gnosus/56url2test28134");
         return headers;
+    }
+
+    @Override
+    protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString =
+                    new String(response.data, HTTP.UTF_8);
+            return Response.success(new JSONArray(jsonString),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
     }
 
 }
